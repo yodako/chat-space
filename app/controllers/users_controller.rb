ddf02195e5f3702f2257410c97710789eb68416c
yodako <yoda@sana.local>
2018-09-08 22:42:47 +0900

    Edit icon link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    user = User.find(current_user)
+    if user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
