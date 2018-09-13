5364a3bb14a992aa89fb7dad9f30b549e05ca3ff
yodako <yoda@sana.local>
2018-09-13 14:39:24 +0900

    routing,controller  add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+   @user = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+   respond_to do |format|
+     format.html
+     format.json
+   end
+  end
+
   def edit
   end
 
