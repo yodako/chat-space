8b491f9ee7f3c513ac8f8886f45eba7498a53632
yodako <yoda@sana.local>
2018-09-14 00:41:36 +0900

    Member  add remove

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,6 +9,16 @@ $(function() {
     $('#user-search-result').append(html);
   }
 
+  function addUser(user_id, user_name) {
+    var html = `
+    <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+      <p class='chat-group-user__name'>${user_name}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+    $('#chat-group-users').append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -32,5 +42,15 @@ $(function() {
     })
   });
 
+  $("#user-search-result").on("click", ".chat-group-user__btn--add", function() {
+    var user_id = $(this).attr("data-user-id");
+    var user_name = $(this).attr("data-user-name");
+    addUser(user_id, user_name);
+    $(this).parent().remove();
+  });
+
+  $(document).on('click','.chat-group-user__btn--remove',function(){
+      $(this).parent().remove();
+  });
 
 });
