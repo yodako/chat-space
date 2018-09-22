9a6de408604c7f188702f045f0acb2635f0bbc59
yodako <yoda@sana.local>
2018-09-22 16:14:22 +0900

    Region Fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['ARTIFACTS_REGION']
+    region:                'ap-northeast-1'
   }
 
   config.fog_directory  = 'yodako-test'
